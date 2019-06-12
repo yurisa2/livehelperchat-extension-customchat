3d975b2e7d196bb2b7ef125450caeb25cd5c29ba
yurisa2-i3wm <yuri@sa2.com.br>
2019-06-12 20:49:31 -0300

    gomenasai

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -11,9 +11,9 @@ class erLhcoreClassExtensionCustomchat {
 		$dispatcher = erLhcoreClassChatEventDispatcher::getInstance();
 		
 		$dispatcher->listen('chat.web_add_msg_admin',array($this,'opMsg'));
-		$dispatcher->listen('chat.auto_responder_triggered',array($this,'opAutoRes'));
-		// $dispatcher->listen('file.uploadfileadmin.file_path',array($this,'uploadFile'));
-		// $dispatcher->listen('file.uploadfile.file_path',array($this,'uploadFile'));
+		// $dispatcher->listen('chat.auto_responder_triggered',array($this,'opAutoRes'));
+		$dispatcher->listen('chat.before_auto_responder_message',array($this,'opAutoRes'));
+		
 	}
 	
 	public function opMsg($params) {
@@ -33,8 +33,25 @@ class erLhcoreClassExtensionCustomchat {
 	}
 	
 	public function opAutoRes($params) {
+	ini_set("display_errors","on");
+		include '../wapp-LHC-Bridge/include/config.php';
+		
+		$params->msg->msg = $params->responder->wait_message;
+		$params->msg->name_support = $params->responder->operator;
+		
+		$data = array('params' => json_encode($params));
+		
+		$options = array(
+			'http' => array(
+				'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+				'method'  => 'POST',
+				'content' => http_build_query($data)
+			)
+		);
+		$context  = stream_context_create($options);
+		$result = file_get_contents($ws_url_base.'/wapp-LHC-Bridge/out_wapp_hook.php', false, $context);
 	
-	file_put_contents("AutoRes.json",json_encode($params));
+	
 	
 	}
 	
